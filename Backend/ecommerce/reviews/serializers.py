e30b86aed4b46f081737f1177745a69681293441
saheer07 <saheerchungat07@gmail.com>
2025-11-11 10:19:23 +0530

    Deleted Backend/ecommerce folder

--- Backend/ecommerce/reviews/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Review
-
-class ReviewSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username', read_only=True)
-
-    class Meta:
-        model = Review
-        fields = ['id', 'product', 'user', 'rating', 'comment', 'created_at']
